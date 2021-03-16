d3adcfc7a95976788997d28df56af2dee340259d
Egor Mateshuk <e.mateshuk@gmail.com>
2021-03-17 01:08:33 +0300

    data_lake_etl init

---
+++ data_lake_etl.py
@@ -0,0 +1,34 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.contrib.operators.dataproc_operator import DataProcHiveOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2012, 1, 1, 0, 0, 0),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=20),
+}
+
+dag = DAG(
+    'data_lake_etl',
+    default_args=default_args,
+    description='Data Lake ETL tasks',
+    schedule_interval=timedelta(years=1),
+)
+
+ods_billing = DataProcHiveOperator(
+    task_id='ods_billing',
+    dag=dag,
+    query="""
+        insert overwrite table ods.billing partition (year='{{ execution_date.year }}') 
+        select * from stg.billing where year(from_unixtime(`timestamp` div 1000)) = {{ execution_date.year }};
+    """,
+    # job_name=GCP_PROJECT_TEMPLATED,
+    cluster_name='cluster-dataproc-m',
+    # region=GCP_REGION_TEMPLATED,
+)
