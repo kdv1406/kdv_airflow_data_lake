03fedb2d28f070e96bd4554e74f3e72a7187d6a3
Egor Mateshuk <e.mateshuk@gmail.com>
2021-03-17 02:03:45 +0300

    Remove comment

--- data_lake_etl.py
+++ data_lake_etl.py
@@ -28,7 +28,6 @@ ods_billing = DataProcHiveOperator(
         insert overwrite table ods.billing partition (year='{{ execution_date.year }}') 
         select * from stg.billing where year(from_unixtime(`timestamp` div 1000)) = {{ execution_date.year }};
     """,
-    # job_name=GCP_PROJECT_TEMPLATED,
     cluster_name='cluster-dataproc',
     region='us-central1',
 )
